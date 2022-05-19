a5f9ce21727dabd7657f18af42c7835298ca1231
PRESSO <user1205@naver.com>
2022-05-19 19:22:50 +0900

    Clear files

--- cafeproject/cafeapp/urls.py
+++ cafeproject/cafeapp/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomeView),
+    path('', views.HomeView, name="home"),
+    path('detail/<int:pk>', views.DetailView, name="detail"),
+
 ]
